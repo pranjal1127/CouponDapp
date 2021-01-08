7271fba395cc8e34dce4068745c525347be79934
pranjal1127 <2018kucp1127@iiitkota.ac.in>
2021-01-08 20:28:24 +0530

    Add typescript Support

---
+++ src/global.ts
@@ -0,0 +1,13 @@
+import { ethers } from "ethers";
+import { CustomProvider } from "eraswap-sdk";
+import { CouponDappCouponDapp } from "./ethereum/CouponDappCouponDapp";
+
+declare global {
+  interface Window {
+    provider: CustomProvider | ethers.providers.JsonRpcProvider;
+    ethereum: ethers.providers.ExternalProvider;
+    couponDappInstance: CouponDappCouponDapp
+    providerESN: CustomProvider | ethers.providers.JsonRpcProvider;
+    wallet: any;
+  }
+}
